b32c7833ab38479e2dcca7175ed5111679230cbb
Lucas <toonerad@hotmail.com>
2018-09-24 02:02:48 +0100

    Create controller

---
+++ app/Controller/PostsController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+
+class PostsController extends AppController
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->loadModel('Post');
+        $this->loadModel('Categorie');
+        $this->loadModel('Comment');
+    }
+
+    public function index(){
+        $posts = $this->Post->last();
+        $this->render('posts.index', compact('posts'));
+    }
+
+    public  function categorie(){
+
+
+        $categorie = $this->Categorie->find($_GET['id']);
+        if ($categorie === false){
+            $this->notFound();
+        }
+        $articles = $this->Post->lastByCategorie($_GET['id']);
+        $categories = $this->Categorie->all();
+        $this->render('posts.categorie', compact('articles', 'categories', 'categorie'));
+
+    }
+
+    public function single(){
+        $article = $this->Post->findWithCategorie($_GET['id']);
+        $comments = $this->Comment->getComments($_GET['id']);
+        $this->render('posts.single', compact('article', 'comments'));
+    }
+
+    public function blog() {
+        $posts = $this->Post->all();
+        $this->render('posts.blog', compact('posts'));
+    }
+
+}
+
---
+++ core/Controller/Controller.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Core\Controller;
+
+class Controller{
+
+    protected $viewPath;
+    protected $template;
+
+    protected function render($view, $variables = [])
+    {
+        ob_start();
+        extract($variables);
+        require ($this->viewPath . str_replace('.', '/', $view) . '.php');
+        $content = ob_get_clean();
+        require ($this->viewPath . 'templates/' . $this->template . '.php');
+    }
+
+
+    protected function notFound(){
+        header('HTTP/1.0 404 Not Found');
+        //header('Location: http://www.google.com/');
+        header('Location:   index.php?p=error');
+    }
+
+    protected function forbidden(){
+        header('HTTP/1.0 403 Forbidden');
+        //die('Acces interdit');
+        header('Location:   index.php?p=error');
+    }
+
+}
