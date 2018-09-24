db4b44d984b9326e07422f2948d0e92ff87090b6
Lucas <toonerad@hotmail.com>
2018-09-24 02:04:41 +0100

    Create PostEntity and CategorieEntity class

---
+++ app/Entity/PostEntity.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Entity;
+
+use Core\Entity\Entity;
+
+class PostEntity extends Entity
+{
+
+    public function getUrl(){
+        return 'index.php?p=posts.single&id='. $this->id;
+    }
+
+    public function getExtrait()
+    {
+        $html = '<p>' . substr($this->content, 0, 100) . '...</p>';
+        return $html;
+
+    }
+
+}
