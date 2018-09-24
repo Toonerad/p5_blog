d1fdba1aab79dbe50e2b8c32f0572bc831499121
Lucas <toonerad@hotmail.com>
2018-09-24 03:47:13 +0100

    Re commit (don't understand why)

---
+++ app/Table/CommentTable.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Table;
+
+use Core\Table\Table;
+
+class CommentTable extends Table
+{
+    protected $table = "comments";
+
+
+    /*
+     * Récupère les commentaires d'un articles
+     */
+    public function getComments($id) {
+        return $this->query("SELECT * FROM comments WHERE articles_id = ? ORDER BY date_added DESC", [$id], true);
+    }
+
+}
